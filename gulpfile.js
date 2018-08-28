e1ee146898be01b24339bfd10a1165137fcc99df
ObemgMCBook <office@spaceweb.studio>
2018-08-28 23:57:26 +0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp      = require('gulp'),
+    livereload  = require('gulp-livereload');
+
+
+gulp.task('watch', function(){
+  livereload.listen();
+  gulp.watch('index.html').on('change', livereload.changed);
+});
+
+gulp.task(
+  'default',
+  gulp.series(
+    'watch'
+  )
+);
