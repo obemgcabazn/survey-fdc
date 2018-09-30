ec73778b8ce2d235c44dc1695a0b4642c5267229
ObemgMCBook <office@spaceweb.studio>
2018-09-30 21:41:17 +0300

    sendpulse condition address books

--- api/mail.php
+++ api/mail.php
@@ -1,5 +1,13 @@
 <?php
 // Получение переменных
+
+/* 
+duration:
+
+0 - не удален
+1 - менее 2х месяцев назад
+2 - более 2х месяцев назад */
+
 if(isset($_POST['duration'])){$duration = htmlspecialchars($_POST['duration']);}
 if(isset($_POST['email'])){$email = htmlspecialchars($_POST['email']);}
 if(isset($_POST['name'])){$name = htmlspecialchars($_POST['name']);}
@@ -17,60 +25,45 @@ $addressBooks = [
   1931826, // 8 Одномоментная, с немедленной нагрузкой, классическая
 ];
 
-if($duration == 0) {
-
-  if(count(array_intersect($tooth, $cond_first)) > 0){
-    // С немедленной нагрузкой, классическая
-    $bookID = $addressBooks[8];
-    echo "8 Одномоментная, с немедленной нагрузкой, классическая";
-  }elseif(count(array_intersect($tooth, $cond_second)) > 0) {
-    
-  }elseif(count(array_intersect($tooth, $cond_third)){
-
-  }else {
-
-  }
-
-} elseif ($duration == 1){
-
-} elseif ($duration == 2) {
-
-} else {
- echo "что-то пошло не так";
-}
-
 $cond_first = array(11, 12, 13, 21, 22, 23, 31, 32, 33, 34, 35, 41, 42, 43, 44, 45);
 $cond_second = array(14, 15, 24, 25);
 $cond_third = array(16, 17, 26, 27, 36,  37, 46, 47);
 
-if($duration == 0 && count(array_intersect($tooth, $cond_first)) > 0){
-  echo 'Одномоментная, с немедленной нагрузкой, классическая';
-} else {
-  echo "нет";
+if( count( array_intersect( $tooth, $cond_second ) ) > 0 ) {
+  if($duration == 0) {
+    // Зуб не удален
+    $bookID = $addressBooks[5];
+    echo "5 Одномоментная, с немедленной нагрузкой, классическая, но может понадобиться костная пластика";
+  } elseif ( $duration == 1 ) {
+    $bookID = $addressBooks[3];
+    echo "3 С немедленной нагрузкой, классическая, может понадобиться костная пластика, но нужно ждать";
+  }else{
+    $bookID = $addressBooks[4];
+    echo "4 С немедленной нагрузкой, классическая, но может понадобиться костная пластика";
+  }
+}elseif( count( array_intersect( $tooth, $cond_first ) ) > 0 ){
+  if ($duration == 0){
+    $bookID = $addressBooks[8];
+    echo "8 Одномоментная, с немедленной нагрузкой, классическая";
+  } elseif ($duration == 1) {
+    $bookID = $addressBooks[6];
+    echo "6 С немедленной нагрузкой, классическая, нужно ждать";
+  } else {
+    $bookID = $addressBooks[7];
+    echo "7 С немедленной нагрузкой, классическая";
+  }
+}else {
+  if ($duration == 0){
+    $bookID = $addressBooks[2];
+    echo "2 Одномоментная, классическая";
+  } elseif ($duration == 1) {
+    $bookID = $addressBooks[0];
+    echo "0 Классическая, но нужно ждать";
+  } else {
+    $bookID = $addressBooks[1];
+    echo "1 Классическая";
+  }
 }
-
-// var_dump($addressBooks);
-
-// $email = htmlspecialchars($_POST['email']);
-// if(isset($_POST['pavilion'])){$pavilion = htmlspecialchars($_POST['pavilion']);}
-// if(isset($_POST['name'])){$name = htmlspecialchars($_POST['name']);}
-// if(isset($_POST['phone'])){$phone = htmlspecialchars($_POST['phone']);}
-
-
-// $username = htmlspecialchars($_POST['name']);
-// $email = htmlspecialchars($_POST['email']);
-
-// if ($email) {
-
-//   $to = 'office@spaceweb.studio'; 
-//   $from='site@spaceweb.studio'; 
-
-//   mail($to, $username, $phone, 'From:'.$from);
-
-//   echo "письмо отправлено";
-
-// }
-
 
   /*
    * SendPulse REST API Usage Example
@@ -119,5 +112,5 @@ $emails = array(
   )
 );
 
-//var_dump($SPApiClient->addEmails($bookID, $emails));
+var_dump($SPApiClient->addEmails($bookID, $emails));
 ?>
